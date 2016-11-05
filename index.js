bc810e04ecd12e3e0cb5d03d4bb1bcce09d15ac4
Kneelawk <jedidiah.pommert@gmail.com>
2016-11-05 16:13:39 -0700

    Basic electron setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const { BrowserWindow, app, ipcMain } = require("electron");
+
+let win;
+
+function openWindow() {
+  win = new BrowserWindow({
+    width: 300,
+    height: 200
+  });
+  win.loadURL(`file://${__dirname}/web/login.html`);
+  win.setMenu(null);
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', openWindow);
+
+app.on('all-window-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activated', () => {
+  if (win === null) {
+    openWindow();
+  }
+});
