a964695e54e000de14c739bb2c3d136a36952cf4
Kneelawk <jedidiah.pommert@gmail.com>
2016-11-19 19:05:38 -0800

    Add downloader

--- app/downloader.js
+++ app/downloader.js
@@ -1,1 +1,50 @@
 const request = require('request');
+const Callback = require('events');
+
+class DownloadCallback extends Callback {}
+
+/*
+ * Downloads a file from url to out.
+ * ulr: Url to download from.
+ * out: WriteStream to be written to.
+ */
+function download(url, out) {
+  let size = 0;
+  let downloaded = 0;
+
+  let callback = new DownloadCallback();
+
+  request(url).on('response' (response) => {
+    if (Math.floor(response.statusCode / 100) == 2) {
+      if (response.headers.includes('content-length')) {
+        size = response.headers['content-length'];
+      }
+      callback.emit('response', response);
+    } else {
+      callback.emit('error', {
+        type: 'bad response code',
+        response
+      });
+    }
+  }).on('data', (chunk) => {
+    callback.emit('progress', {
+      progress: downloaded,
+      outOf: size
+    });
+  }).on('end', () => {
+    callback.emit('end');
+  }).on('error', (error) => {
+    callback.emit('error', error);
+  });
+
+  return callback;
+}
+
+function getFileName(url) {
+  return url.slice(url.lastIndexOf('/') + 1);
+}
+
+module.exports = {
+  download,
+  getFileName
+};
